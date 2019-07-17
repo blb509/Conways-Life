d99adbd7a54ac77591c09f0e7943cd95a70132dd
Brandon L. Borrero <borrero509@outlook.com>
2019-07-17 16:10:48 -0700

    grid rendered and logic for game almost finished

---
+++ cgol/src/App.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import Cell from "./Cell.js";
+import Rules from "./Rules.js";
+import "./App.css";
+
+export default class App extends Component {
+  state = {
+    rules: new Rules(),
+    size: [40, 20],
+    running: false
+  };
+
+  handleRowChange(e) {
+    if (!this.state.running) {
+      let actualSize = this.state.size;
+
+      if (e.target.value < 20) actualSize[1] = e.target.value;
+      else actualSize[1] = 20;
+
+      this.setState({
+        size: actualSize
+      });
+
+      this.renderBoard();
+    }
+  }
+
+  handleColumnChange(e) {
+    if (!this.state.running) {
+      let actualSize = this.state.size;
+      if (e.target.value < 40) actualSize[0] = e.target.value;
+      else actualSize[0] = 40;
+
+      this.setState({
+        size: actualSize
+      });
+
+      this.renderBoard();
+    }
+  }
+
+  startGame() {
+    if (!this.state.running) {
+      this.setState(
+        {
+          running: true
+        },
+        () => {
+          this.intervalRef = setInterval(() => this.runGame(), 10);
+        }
+      );
+    }
+  }
+
+  stopGame() {
+    this.setState(
+      {
+        running: false
+      },
+      () => {
+        if (this.intervalRef) {
+          clearInterval(this.intervalRef);
+        }
+      }
+    );
+  }
+
+  runGame() {
+    this.setState({
+      rules: this.state.rules.addGeneration()
+    });
+  }
+
+  renderGrid() {
+    let grid = [];
+    let cellRow = [];
+
+    for (let i = 0; i < this.state.size[0]; i++) {
+      for (let j = 0; j < this.state.size[1]; j++) {
+        cellRow.push(<Cell key={[i, j]} />);
+      }
+      grid.push(
+        <div className="row" key={i}>
+          {cellRow}
+        </div>
+      );
+      cellRow = [];
+    }
+
+    return grid;
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="controls">
+          <div className="innerControls">
+            <label className="label">
+              Rows:
+              <input
+                className="input"
+                type="text"
+                value={this.state.size[1]}
+                onChange={this.handleRowChange}
+              />
+            </label>
+            <label className="label">
+              Columns:
+              <input
+                className="input"
+                type="text"
+                value={this.state.size[0]}
+                onChange={this.handleColumnChange}
+              />
+            </label>
+          </div>
+          <div className="buttons">
+            <button className="submit" onClick={this.startGame}>
+              Start
+            </button>
+            <button className="submit" onClick={this.stopGame}>
+              Stop
+            </button>
+          </div>
+          Generation:
+        </div>
+        <div className="gridContainer">{this.renderGrid()}</div>
+      </div>
+    );
+  }
+}
